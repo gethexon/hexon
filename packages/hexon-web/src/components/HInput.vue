ff05faf2b1b1495f47c4c2eb22aec9227a5f2356
winwin2011 <yjh2011@live.com>
2021-09-18 02:47:06 +0800

    feat(web): h-input-clearable

--- packages/hexon-web/src/components/HInput.vue
+++ packages/hexon-web/src/components/HInput.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts">
-import { toRefs } from "@vue/reactivity";
+import { computed, toRefs } from "@vue/reactivity";
+import { Dismiss16Filled } from "@vicons/fluent";
 import HVerticalCenter from "./HVerticalCenter.vue";
+import HIcon from "./HIcon.vue";
 
 const props = withDefaults(
-  defineProps<{ modelValue: string; placeholder?: string }>(),
-  { placeholder: "" }
+  defineProps<{
+    modelValue: string;
+    placeholder?: string;
+    clearable?: boolean;
+  }>(),
+  { placeholder: "", clearable: false }
 );
 const emit = defineEmits<{ (e: "update:modelValue", value: string): void }>();
 
-const { modelValue, placeholder } = toRefs(props);
+const { modelValue, placeholder, clearable } = toRefs(props);
+const showSuffix = computed(() => clearable.value && modelValue.value);
 
 const requestChange = (value: string) => emit("update:modelValue", value);
 const onInput = (e: InputEvent) =>
   requestChange((e.target as HTMLInputElement)?.value);
+const onClear = () => requestChange("");
 </script>
 <template>
   <label class="container">
@@ -22,6 +30,11 @@ const onInput = (e: InputEvent) =>
       </HVerticalCenter>
     </div>
     <input :value="modelValue" @input="onInput" :placeholder="placeholder" />
+    <div class="suffix" v-if="showSuffix" @click="onClear">
+      <HIcon>
+        <Dismiss16Filled />
+      </HIcon>
+    </div>
   </label>
 </template>
 <style scoped lang="less">
@@ -34,11 +47,20 @@ const onInput = (e: InputEvent) =>
   padding: 0 12px;
   cursor: text;
   overflow: hidden;
-  .prefix {
-    margin-right: 4px;
+  .prefix,
+  .suffix {
     display: flex;
     align-items: center;
     height: 100%;
+  }
+  .prefix {
+    margin-right: 4px;
+  }
+  .suffix {
+    margin-left: 4px;
+    & span {
+      cursor: pointer;
+    }
   }
   input {
     flex: 1;
