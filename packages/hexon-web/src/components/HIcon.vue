cdf6ee11f6ffba2c0de6c1d6af86714a968f0d66
winwin2011 <yjh2011@live.com>
2021-09-19 00:15:35 +0800

    feat(web): h-icon clickable

--- packages/hexon-web/src/components/HIcon.vue
+++ packages/hexon-web/src/components/HIcon.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
-import { toRefs } from "@vue/reactivity";
+import { computed, toRefs } from "@vue/reactivity";
 import { Icon } from "@vicons/utils";
 
-const props = withDefaults(defineProps<{ size?: string | number }>(), {
-  size: "1em",
+const props = withDefaults(
+  defineProps<{ size?: string | number; clickable: boolean }>(),
+  {
+    size: "1em",
+    clickable: false,
+  }
+);
+const { size, clickable } = toRefs(props);
+const classes = computed(() => {
+  return { clickable: clickable.value };
 });
-const { size } = toRefs(props);
 </script>
 <template>
-  <Icon :size="size"><slot></slot></Icon>
+  <Icon class="h-icon" :size="size" :class="classes"><slot></slot></Icon>
 </template>
+<style scoped lang="less">
+.h-icon.clickable {
+  cursor: pointer;
+}
+</style>
