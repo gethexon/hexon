e18bb119cf75c65e7720744f64c74f6c63e3f624
winwin2011 <yjh2011@live.com>
2021-09-21 13:09:18 +0800

    feat(vue): h-toolbar h-title

---
+++ packages/hexon-web/src/components/HTitle.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HToolbar from "./HToolbar.vue";
+</script>
+<template>
+  <HToolbar name="HToolbar"
+    ><span style="font-size: 1.5rem; color: var(--color-foreground-2)"
+      >Hexon</span
+    ></HToolbar
+  >
+</template>
---
+++ packages/hexon-web/src/components/HToolbar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+<template>
+  <div
+    style="
+      height: 50px;
+      display: flex;
+      align-items: center;
+      user-select: none;
+      padding: 0 16px;
+    "
+  >
+    <slot></slot>
+  </div>
+</template>
