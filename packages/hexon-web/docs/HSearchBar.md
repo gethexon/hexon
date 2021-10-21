d656078d570e9a76cb77cb2f81f0ee7c58677aed
winwin2011 <yjh2011@live.com>
2021-10-22 05:50:13 +0800

    feat: h-search-bar

---
+++ packages/hexon-web/docs/HSearchBar.md
@@ -0,0 +1,14 @@
+# HSearchBar
+
+## 基础
+
+<div style="background-color:var(--color-background-c3);display:inline-block;width:300px">
+<HSearchBar v-model="value"></HSearchBar>
+</div>
+
+<script setup>
+import { ref } from 'vue'
+import HSearchBar from '../src/components/HSearchBar.vue'
+import HButton from '../src/components/HButton.vue'
+const value = ref('')
+</script>
---
+++ packages/hexon-web/src/components/HSearchBar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import HToolbar from "./HToolbar.vue";
+import HInput from "./HInput.vue";
+import HButton from "./HButton.vue";
+import HIcon from "./HIcon.vue";
+import { HIconName } from "./HIconName";
+import { toRefs } from "vue-demi";
+
+const props = defineProps<{
+  modelValue: string;
+}>();
+const emits = defineEmits<{
+  (e: "update:modelValue", value: string): void;
+  (e: "on-add"): void;
+}>();
+const { modelValue } = toRefs(props);
+const onInput: any = (value: string) => emits("update:modelValue", value);
+</script>
+<template>
+  <HToolbar>
+    <div class="flex w-full px-6">
+      <div class="flex-1"></div>
+      <!-- <HInput :modelValue="modelValue" @update:model-value="onInput" clearable>
+      </HInput>
+      <HButton class="ml-2" round><HIcon :name="HIconName.Search" /></HButton> -->
+      <HButton class="ml-2" round @click="emits('on-add')"
+        ><HIcon :name="HIconName.Add"
+      /></HButton>
+    </div>
+  </HToolbar>
+</template>
