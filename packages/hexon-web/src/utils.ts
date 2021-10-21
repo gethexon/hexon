238fcca376ccdf7d313fc972b8567e61a0730c47
winwin2011 <yjh2011@live.com>
2021-10-22 05:26:21 +0800

    refactor: 重写 list2object 类型定义

--- packages/hexon-web/src/utils.ts
+++ packages/hexon-web/src/utils.ts
@@ -2,25 +2,30 @@ export const forceReloadWindow = () => {
   window.onbeforeunload = () => {};
   window.location.reload();
 };
+
 export const DEV = process.env.NODE_ENV !== "production";
 
-type ListItem<T1, T2 extends string> = T1 & {
-  [key in T2]: string;
-};
-
-export function list2object<T1, T2 extends string>(
-  list: ListItem<T1, T2>[],
-  key: T2
-): { [key: string]: T1 } {
-  const o: { [key: string]: ListItem<T1, T2> } = {};
+export function list2object<
+  T extends {
+    [key in K]: string;
+  },
+  K extends string
+>(list: T[], key: K): { [key: string]: T } {
+  const o: { [key: string]: T } = {};
   list.forEach((item) => (o[item[key]] = item));
   return o;
 }
-export function object2list<T1, T2 extends string>(
-  object: {
-    [key: string]: ListItem<T1, T2>;
+
+export function object2list<
+  T extends {
+    [key in K]: string;
   },
-  key: T2
-): T1[] {
+  K extends string
+>(
+  object: {
+    [key: string]: T;
+  },
+  key: K
+): T[] {
   return Object.entries(object).map(([key, value]) => value);
 }
