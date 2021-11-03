3ac81a189ace10807e2829a26651201320169336
winwin2011 <yjh2011@live.com>
2021-11-03 18:03:26 +0800

    fix: path 为空时新建文章崩溃

--- packages/server/src/hexo/controller.js
+++ packages/server/src/hexo/controller.js
@@ -1,7 +1,7 @@
-const Joi = require('joi')
-const DI = require('../util/di')
-const { IHexo } = require('./core/hexo')
-const { search } = require('./search')
+const Joi = require("joi");
+const DI = require("../util/di");
+const { IHexo } = require("./core/hexo");
+const { search } = require("./search");
 
 // #region validate
 exports.v = {
@@ -10,142 +10,147 @@ exports.v = {
     watch: Joi.boolean(),
     bail: Joi.boolean(),
     force: Joi.boolean(),
-    concurrency: Joi.boolean()
+    concurrency: Joi.boolean(),
   }),
   deploy: Joi.object({
-    generate: Joi.boolean()
+    generate: Joi.boolean(),
   }),
   new: Joi.object({
     title: Joi.string().required(),
     layout: Joi.string(),
     path: Joi.string(),
     slug: Joi.string(),
-    replace: Joi.boolean()
+    replace: Joi.boolean(),
   }),
   update: Joi.object({
     id: Joi.string().required(),
     page: Joi.boolean(),
-    obj: Joi.object().required()
+    obj: Joi.object().required(),
   }),
   delete: Joi.object({
     id: Joi.string().required(),
-    page: Joi.boolean()
+    page: Joi.boolean(),
   }),
   publish: Joi.object({
-    id: Joi.string().required()
-  })
-}
+    id: Joi.string().required(),
+  }),
+};
 exports.qv = {
   search: Joi.object({
     query: Joi.string(),
-    mode: Joi.string()
-  })
-}
+    mode: Joi.string(),
+  }),
+};
 // #endregion
 
 // #region actions
 exports.generate = async (ctx, next) => {
-  const hexo = DI.inject(IHexo)
-  await hexo.generate(ctx.request.body)
-  ctx.status = 200
-}
+  const hexo = DI.inject(IHexo);
+  await hexo.generate(ctx.request.body);
+  ctx.status = 200;
+};
 exports.deploy = async (ctx, next) => {
-  const hexo = DI.inject(IHexo)
-  await hexo.deploy(ctx.request.body)
-  ctx.status = 200
-}
+  const hexo = DI.inject(IHexo);
+  await hexo.deploy(ctx.request.body);
+  ctx.status = 200;
+};
 exports.clean = async (ctx, next) => {
-  const hexo = DI.inject(IHexo)
-  await hexo.clean()
-  ctx.status = 200
-}
+  const hexo = DI.inject(IHexo);
+  await hexo.clean();
+  ctx.status = 200;
+};
 // #endregion
 
 // #region list
 exports.listPost = async (ctx, next) => {
-  const hexo = DI.inject(IHexo)
-  ctx.body = await hexo.listPost()
-}
+  const hexo = DI.inject(IHexo);
+  ctx.body = await hexo.listPost();
+};
 exports.listPage = async (ctx, next) => {
-  const hexo = DI.inject(IHexo)
-  ctx.body = await hexo.listPage()
-}
+  const hexo = DI.inject(IHexo);
+  ctx.body = await hexo.listPage();
+};
 exports.listTag = async (ctx, next) => {
-  const hexo = DI.inject(IHexo)
-  ctx.body = await hexo.listTag()
-}
+  const hexo = DI.inject(IHexo);
+  ctx.body = await hexo.listTag();
+};
 exports.listCategory = async (ctx, next) => {
-  const hexo = DI.inject(IHexo)
-  ctx.body = await hexo.listCategory()
-}
+  const hexo = DI.inject(IHexo);
+  ctx.body = await hexo.listCategory();
+};
 // #endregion
 
 // #region new
 exports.new = async (ctx, next) => {
-  const hexo = DI.inject(IHexo)
-  const { title, layout, path, slug, replace } = ctx.request.body
-  const res = await hexo.new(title, { layout, path: path.split('\\').join('/'), slug, replace })
-  ctx.status = 200
-  ctx.body = res
-}
+  const hexo = DI.inject(IHexo);
+  const { title, layout, path, slug, replace } = ctx.request.body;
+  const res = await hexo.new(title, {
+    layout,
+    path: path?.split("\\").join("/"),
+    slug,
+    replace,
+  });
+  ctx.status = 200;
+  ctx.body = res;
+};
 // #endregion
 
 // #region update
 exports.update = async (ctx, next) => {
-  const hexo = DI.inject(IHexo)
-  const { id, page, obj } = ctx.request.body
-  const res = await hexo.write(id, obj, page)
-  ctx.status = 200
-  ctx.body = res
-}
+  const hexo = DI.inject(IHexo);
+  const { id, page, obj } = ctx.request.body;
+  const res = await hexo.write(id, obj, page);
+  ctx.status = 200;
+  ctx.body = res;
+};
 // #endregion
 
 // #region delete
 exports.delete = async (ctx, next) => {
-  const hexo = DI.inject(IHexo)
-  const { id, page } = ctx.request.body
-  await hexo.delete(id, page)
-  ctx.status = 200
-}
+  const hexo = DI.inject(IHexo);
+  const { id, page } = ctx.request.body;
+  await hexo.delete(id, page);
+  ctx.status = 200;
+};
 // #endregion
 
 // #region publish
 exports.publish = async (ctx, next) => {
-  const hexo = DI.inject(IHexo)
-  const { id } = ctx.request.body
-  const res = await hexo.publish(id)
-  ctx.body = res
-  ctx.status = 200
-}
+  const hexo = DI.inject(IHexo);
+  const { id } = ctx.request.body;
+  const res = await hexo.publish(id);
+  ctx.body = res;
+  ctx.status = 200;
+};
 // #endregion
 
 // #region git
 exports.gitSync = async (ctx, next) => {
-  const hexo = DI.inject(IHexo)
-  await hexo.gitSync()
-  ctx.status = 200
-}
+  const hexo = DI.inject(IHexo);
+  await hexo.gitSync();
+  ctx.status = 200;
+};
 exports.gitSave = async (ctx, next) => {
-  const hexo = DI.inject(IHexo)
-  await hexo.gitSave()
-  ctx.status = 200
-}
+  const hexo = DI.inject(IHexo);
+  await hexo.gitSave();
+  ctx.status = 200;
+};
 exports.notGitRepo = async (ctx, next) => {
   try {
-    await next()
+    await next();
   } catch (e) {
     // TODO 其他平台和git版本如何检测
-    if (e.message.indexOf('not a git repo') > -1) {
-      ctx.status = 503
+    if (e.message.indexOf("not a git repo") > -1) {
+      ctx.status = 503;
       ctx.body = {
-        message: 'not a git repo'
-      }
-    } else throw e
+        message: "not a git repo",
+      };
+    } else throw e;
   }
-}
+};
 // #endregion
 // #region search
 exports.search = async (ctx, next) => {
-  ctx.body = await search(decodeURIComponent(ctx.query.query), ctx.query.mode)
-}
+  ctx.body = await search(decodeURIComponent(ctx.query.query), ctx.query.mode);
+};
 // #endregion
