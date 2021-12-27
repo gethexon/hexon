fe421061f5a0198ca8a1618fb582badf8d87b707
winwin2011 <yjh2011@live.com>
2021-12-28 05:26:35 +0800

    chore: better doc nav name

--- packages/hexon-web/.demo/utils.ts
+++ packages/hexon-web/.demo/utils.ts
@@ -2,7 +2,15 @@ export const modules = import.meta.glob("../src/**/demo/*.demo.vue")
 export const getNameFromPath = (path: string) => {
   return path
     .split("../src/")[1]
+    .split("lib/")
+    .join("")
+    .split("components/")
+    .join("")
+    .split("ui/")
+    .join("")
     .split("/demo/")
     .join("/")
     .split(".demo.vue")[0]
+    .split("/default")
+    .join("")
 }
