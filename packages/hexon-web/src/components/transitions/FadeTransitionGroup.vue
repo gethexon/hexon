a69c0bddf38ec781df8cfaf067aa53e4133d67c2
winwin2011 <yjh2011@live.com>
2021-12-28 00:52:46 +0800

    feat: transition

---
+++ packages/hexon-web/src/components/transitions/FadeTransitionGroup.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import anime from "animejs"
+
+const props = withDefaults(defineProps<{ duration: number }>(), {
+  duration: 100,
+})
+
+function enter(el: Element, done: () => void) {
+  anime({
+    targets: el,
+    opacity: 1,
+    duration: props.duration,
+    complete: done,
+  })
+}
+function beforeEnter(el: Element) {
+  anime.set(el, {
+    opacity: 0,
+  })
+}
+function leave(el: Element, done: () => void) {
+  anime({
+    targets: el,
+    opacity: 0,
+    duration: props.duration,
+    complete: done,
+  })
+}
+</script>
+
+<template>
+  <TransitionGroup @enter="enter" @before-enter="beforeEnter" @leave="leave">
+    <slot></slot>
+  </TransitionGroup>
+</template>
--- packages/hexon-web/src/lib/dialog/src/DialogContainer.vue
+++ packages/hexon-web/src/lib/dialog/src/DialogContainer.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import { useDialog } from "."
 import ClassProvider from "~/ClassProvider.vue"
+import FadeTransitionGroup from "./FadeTransitionGroup.vue"
 const { dialogs } = useDialog()
 </script>
 <template>
   <Teleport to="body">
     <ClassProvider>
-      <slot v-for="item in dialogs" :key="item.id" :data="item"> </slot>
+      <FadeTransitionGroup>
+        <div v-for="item in dialogs" :key="item.id">
+          <slot :data="item"> </slot>
+        </div>
+      </FadeTransitionGroup>
     </ClassProvider>
   </Teleport>
 </template>
