a531eedfa284f41dfe3cf29b0d1fa227e42097cc
winwin2011 <yjh2011@live.com>
2022-01-20 06:14:49 +0800

    chore(typedef): build

---
+++ packages/hexon-typedef/dist/index.d.ts
@@ -0,0 +1,14 @@
+export declare enum ERROR_CODE {
+    E_INIT = 0,
+    E_INITIATING = 1,
+    E_UNKNOWN = 2,
+    E_NOT_FOUND = 3
+}
+export interface IErrorResponse {
+    code: ERROR_CODE;
+    message: string;
+}
+export declare function createErrorResponse(code: ERROR_CODE, message: string): {
+    code: ERROR_CODE;
+    message: string;
+};
---
+++ packages/hexon-typedef/dist/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+exports.ERROR_CODE = void 0;
+(function (ERROR_CODE) {
+    ERROR_CODE[ERROR_CODE["E_INIT"] = 0] = "E_INIT";
+    ERROR_CODE[ERROR_CODE["E_INITIATING"] = 1] = "E_INITIATING";
+    ERROR_CODE[ERROR_CODE["E_UNKNOWN"] = 2] = "E_UNKNOWN";
+    ERROR_CODE[ERROR_CODE["E_NOT_FOUND"] = 3] = "E_NOT_FOUND";
+})(exports.ERROR_CODE || (exports.ERROR_CODE = {}));
+function createErrorResponse(code, message) {
+    return { code, message };
+}
+
+exports.createErrorResponse = createErrorResponse;
