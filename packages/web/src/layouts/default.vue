590626443a5d64974f3c8ae7d244c2b9fb9933e2
winwin2011 <yjh2011@live.com>
2022-04-09 15:41:50 +0800

    fix: settings unset

--- packages/web/src/layouts/default.vue
+++ packages/web/src/layouts/default.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { onMounted } from "vue"
 import { RouterView } from "vue-router"
+import { useSettingsStore } from "~/store/settings"
+const settings = useSettingsStore()
 onMounted(() => {
+  settings.load()
   console.log("default layout used")
 })
 </script>
