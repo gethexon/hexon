ae4a26eb6c92d7b07a9d482c9cb96d1fc48145dc
winwin2011 <yjh2011@live.com>
2024-07-25 14:51:33 +0800

    chore: add log and eslint

--- server-shared/src/account-storage-service.ts
+++ server-shared/src/account-storage-service.ts
@@ -1,7 +1,7 @@
-import { SHA1 } from "crypto-js"
-import { inject, injectable, singleton } from "tsyringe"
-import { StorageService } from "./storage-service"
-import { LogService } from "./log-service"
+import { SHA1 } from 'crypto-js'
+import { inject, injectable, singleton } from 'tsyringe'
+import { StorageService } from './storage-service'
+import { LogService } from './log-service'
 
 interface IUserInfo {
   password: string
@@ -9,54 +9,67 @@ interface IUserInfo {
 }
 
 export class BasicAuthError extends Error {
-  public name = "BasicAuthError"
+  public name = 'BasicAuthError'
 }
 
 @injectable()
 @singleton()
 export class AccountService {
-  public static KEY = "userinfo" as const
+  public static KEY = 'userinfo' as const
   constructor(
     @inject(StorageService) private _storage: StorageService,
-    @inject(LogService) private _logService: LogService
+    @inject(LogService) private _logService: LogService,
   ) {
-    this._logService.setScope("account-service")
+    this._logService.setScope('account-service')
   }
+
   private _encrypt(raw: string) {
     return SHA1(raw).toString()
   }
+
   private _toStorage(info: IUserInfo) {
     this._storage.set(AccountService.KEY, info)
   }
+
   private _fromStorage(): IUserInfo {
-    const { username = "", password = "" } =
-      this._storage.get<IUserInfo>(AccountService.KEY) || {}
+    const { username = '', password = '' }
+      = this._storage.get<IUserInfo>(AccountService.KEY) || {}
     return { username, password }
   }
+
   setUserInfo(username: string, password: string) {
     this._storage.set(AccountService.KEY, {
       username,
       password: this._encrypt(password),
     })
+    this._logService.log('set user info: ', username)
   }
+
   getUsername() {
     return this._fromStorage().username
   }
+
   setUsername(username: string) {
     const info = this._fromStorage()
     info.username = username
     this._toStorage(info)
+    this._logService.log('set username: ', username)
   }
+
   setPassword(password: string) {
     const info = this._fromStorage()
     info.password = this._encrypt(password)
     this._toStorage(info)
+    this._logService.log('set password')
   }
-  setEncrptedPassword(password: string) {
+
+  setEncryptedPassword(password: string) {
     const info = this._fromStorage()
     info.password = password
     this._toStorage(info)
+    this._logService.log('set encrypted password')
   }
+
   verify(username: string, password: string) {
     const info = this._fromStorage()
     if (username !== info.username) {
